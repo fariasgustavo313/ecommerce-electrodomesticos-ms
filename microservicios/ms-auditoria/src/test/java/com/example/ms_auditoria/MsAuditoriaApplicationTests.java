6fdb1c3ff0ded5b893e50678e23a9885d7852b84
Gustavo Farias <ext_fargusta@mercadolibre.com>
2025-07-22 18:39:57 -0300

    proyecto base de microservicios

---
+++ microservicios/ms-auditoria/src/test/java/com/example/ms_auditoria/MsAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_auditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
