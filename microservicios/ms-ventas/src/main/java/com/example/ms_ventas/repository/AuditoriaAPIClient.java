a968154c42624033c69ae2226ef405bf993ef52f
Gustavo Farias <ext_fargusta@mercadolibre.com>
2025-07-23 12:51:38 -0300

    Creacion de AuditoriaAPIClient en el microservicio Venta

--- microservicios/ms-ventas/src/main/java/com/example/ms_ventas/repository/AuditoriaAPIClient.java
+++ microservicios/ms-ventas/src/main/java/com/example/ms_ventas/repository/AuditoriaAPIClient.java
@@ -1,11 +1,13 @@
 package com.example.ms_ventas.repository;
 
+import com.example.ms_ventas.dto.EventoAuditoriaDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "ms-auditoria")
 public interface AuditoriaAPIClient {
 
     @PostMapping("/auditoria")
-
+    EventoAuditoriaDTO registrarEvento(@RequestBody EventoAuditoriaDTO evento);
 }
