08a4beba7f722dc9bbbe982da4d00357a04fc38e
Gustavo Farias <ext_fargusta@mercadolibre.com>
2025-07-23 00:38:23 -0300

    Creacion del model del microservicio Usuarios

---
+++ microservicios/ms-usuarios/src/main/java/com/example/ms_usuarios/model/Usuario.java
@@ -0,0 +1,74 @@
+package com.example.ms_usuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String direccion;
+    private String dni;
+
+    public Usuario(Long id, String nombre, String email, String direccion, String dni) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+        this.dni = dni;
+    }
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
