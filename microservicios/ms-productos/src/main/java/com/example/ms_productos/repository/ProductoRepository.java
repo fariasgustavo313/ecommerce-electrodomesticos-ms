3627bfb5286e411a89942090de6d0affd224fa1b
Gustavo Farias <ext_fargusta@mercadolibre.com>
2025-07-23 01:52:51 -0300

    Creacion del repository del microservicio Productos

---
+++ microservicios/ms-productos/src/main/java/com/example/ms_productos/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.example.ms_productos.repository;
+
+import com.example.ms_productos.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
